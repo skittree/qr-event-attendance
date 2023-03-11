f6284f1a2354d910755f191f013ec97b1e800db0
Arsenii Osokin <a.osokin.sas@gmail.com>
2023-03-12 01:53:10 +0500

    Credentials

---
+++ register_app/Services/IFormsService.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using register_app.Data;
+using Google.Apis.Forms.v1;
+
+namespace register_app.Services
+{
+    public interface IFormsService
+    {
+    }
+
+    public class FormService : IFormsService
+    {
+        private ApplicationDbContext Context { get; }
+        private IMapper Mapper { get; }
+        private UserManager<IdentityUser> UserManager { get; }
+
+
+        public FormService(ApplicationDbContext context,
+            IMapper mapper,
+            UserManager<IdentityUser> userManager)
+        {
+            Context = context;
+            Mapper = mapper;
+            UserManager = userManager;
+        }
+
+
+        public void CreateForm()
+        {
+            
+        }
+    }
+}
