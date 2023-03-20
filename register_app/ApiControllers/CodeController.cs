94c2c51615e8bc898f5fa67487cc540c2da90e9c
skittree <pdimonn@mail.ru>
2023-03-20 14:22:03 +0500

    api for receiving QR-code thru POST

---
+++ register_app/ApiControllers/CodeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using register_app.DtoModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace register_app.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CodeController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post(CodeDto model)
+        {
+            if (model == null)
+            {
+                return BadRequest("object was null");
+            }
+            try
+            {
+                Console.WriteLine("received");
+                return Ok();
+            }
+            catch (ArgumentException ae)
+            {
+                return BadRequest(ae.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ register_app/DtoModels/CodeDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using System.Text.Json.Serialization;
+
+namespace register_app.DtoModels
+{
+    public class CodeDto
+    {
+        [JsonPropertyName("key")]
+        public string Key { get; set; }
+    }
+}
